31b83b27356a2d61538d17fb1446a09fc70b39bc
Katie Bridson <kbridson@memphis.edu>
2024-04-17 15:40:32 -0500

    Index and show pages working, no link

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Car
+
 
 # Create your views here.
+def cars_list(request):
+    cars = Car.objects.all()
+    return render(request, "cars/index.html", {"cars": cars})
+
+
+def car_detail(request, id):
+    car = get_object_or_404(Car, id=id)
+    return render(request, "cars/show.html", {"car": car})
